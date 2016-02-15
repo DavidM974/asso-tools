5daa071ae5b1503e47b2528d5d29cebf3ed326e1
David <dav.michel974@gmail.com>
2016-02-15 16:41:34 +0400

    Add pwd in edit prole and user and add default avatar img

--- src/MSI/UserBundle/Controller/EditUserController.php
+++ src/MSI/UserBundle/Controller/EditUserController.php
@@ -2,86 +2,96 @@
 
 namespace MSI\UserBundle\Controller;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use FOS\UserBundle\Controller\RegistrationController as BaseController;
 use Symfony\Component\HttpFoundation\Request;
-use FOS\UserBundle\FOSUserEvents;
-use FOS\UserBundle\Event\FormEvent;
-use FOS\UserBundle\Event\GetResponseUserEvent;
-use FOS\UserBundle\Event\FilterUserResponseEvent;
-use \MSI\UserBundle\Form\Type\EditUserFormType;
 
-class EditUserController extends BaseController
-{
-    
-    public function editAction(Request $request, $id)
-    {
 
-            $userManager = $this->container->get('fos_user.user_manager');
-            $userToEdit = $userManager->findUserBy(array('id' => $id));
-            $formFactory = $this->container->get('msi_user.edit.form.factory');
-            $form = $formFactory->createForm();
-            $form->setData($userToEdit);
-            
-            $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
-            $formAvatar->setData($userToEdit);
-            
+class EditUserController extends BaseController {
 
-            // Vérifie si je suis bien en Post et valide le formulaire
-            if($form->handleRequest($request)->isValid())
-            {
-                $file = $userToEdit->getImage()->getImageFile();
-                if ($file != null) {
-                    $fileName = md5(uniqid()).'.'.$file->guessExtension();
-                    $userToEdit->getImage()->setImageName($fileName);
-                    $userToEdit->getImage()->setAlt('avatar '.$userToEdit->getUsername());
-                }
-                $userManager->updateUser($userToEdit);
-                  // Move the file to the directory where brochures are stored
-              /*  $imageUserDir = $this->container->getParameter('kernel.root_dir').'/../web/public/images/users';
-                $file->move($imageUserDir, $fileName);*/
-                $url = $this->generateUrl('msi_user_admin_edit_user',  array('id' => $userToEdit->getId()));
-                $session = $request->getSession();
-                $session->getFlashBag()->add('info_user_edit', 
-                        $this->get('translator')->trans('msi.core.admin.edit.user.info',  array('%username%' => $userToEdit->getUsername()) , 'Admin')
-                );
- 
-                return $this->redirect($url);
-            }
-        
-        return $this->render('MSIUserBundle:Profile:edit_user.html.twig', array('form' => $form->createView(),'formAvatar' => $formAvatar->createView(), 'userToEdit' => $userToEdit, 'edit_user' => true));
+    public function editAction(Request $request, $id) {
+
+        $userManager = $this->container->get('fos_user.user_manager');
+        $userToEdit = $userManager->findUserBy(array('id' => $id));
+        $formFactory = $this->container->get('msi_user.edit.form.factory');
+        $form = $formFactory->createForm();
+        $form->setData($userToEdit);
+
+        $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
+        $formAvatar->setData($userToEdit);
+        //init the user password edit form
+        $formPwd = $this->container->get('msi_user.password.form.factory')->createForm();
+        $formPwd->setData($userToEdit);
+
+        // Vérifie si je suis bien en Post et valide le formulaire
+        if ($form->handleRequest($request)->isValid()) {
+            $userManager->updateUser($userToEdit);
+            $url = $this->generateUrl('msi_user_admin_edit_user', array('id' => $userToEdit->getId()));
+            $session = $request->getSession();
+            $session->getFlashBag()->add('info_user_edit', $this->get('translator')->trans('msi.core.admin.edit.user.info', array('%username%' => $userToEdit->getUsername()), 'Admin')
+            );
+
+            return $this->redirect($url);
+        }
+
+        return $this->render('MSIUserBundle:Profile:edit_user.html.twig', array('form' => $form->createView(), 'formAvatar' => $formAvatar->createView(), 'formPwd' => $formPwd->createView(), 'userToEdit' => $userToEdit, 'edit_user' => true));
     }
-    
-    public function editAvatarAction(Request $request, $id)
-    {
-            $userManager = $this->container->get('fos_user.user_manager');
-            $userToEdit = $userManager->findUserBy(array('id' => $id));
-            
 
-            $formUser = $this->container->get('msi_user.edit.form.factory')->createForm();
-            $formUser->setData($userToEdit);
-            
-            $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
-            $formAvatar->setData($userToEdit);
-            if($formAvatar->handleRequest($request)->isValid())
-            {
-            
-                $userManager->updateUser($userToEdit);
-                  // Move the file to the directory where brochures are stored
-              /*  $imageUserDir = $this->container->getParameter('kernel.root_dir').'/../web/public/images/users';
-                $file->move($imageUserDir, $fileName);*/
-                $url = $this->generateUrl('msi_user_admin_edit_avatar_user',  array('id' => $userToEdit->getId()));
-                $session = $request->getSession();
-                $session->getFlashBag()->add('info_user_edit', 
-                        $this->get('translator')->trans('msi.core.admin.edit.user.info',  array('%username%' => $userToEdit->getUsername()) , 'Admin')
-                );
- 
-                return $this->redirect($url);
-            }
-            return $this->render('MSIUserBundle:Profile:edit_user.html.twig', array('form' => $formUser->createView(), 
-                                                                                    'formAvatar' => $formAvatar->createView(), 
-                                                                                    'userToEdit' => $userToEdit, 
-                                                                                    'edit_avatar' => true));
+    public function editAvatarAction(Request $request, $id) {
+        $userManager = $this->container->get('fos_user.user_manager');
+        $userToEdit = $userManager->findUserBy(array('id' => $id));
+
+
+        $formUser = $this->container->get('msi_user.edit.form.factory')->createForm();
+        $formUser->setData($userToEdit);
+        $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
+        $formAvatar->setData($userToEdit);
+        //init the user password edit form
+        $formPwd = $this->container->get('msi_user.password.form.factory')->createForm();
+        $formPwd->setData($userToEdit);
+        if ($formAvatar->handleRequest($request)->isValid()) {
+
+            $userManager->updateUser($userToEdit);
+            $url = $this->generateUrl('msi_user_admin_edit_avatar_user', array('id' => $userToEdit->getId()));
+            $session = $request->getSession();
+            $session->getFlashBag()->add('info_user_edit', $this->get('translator')->trans('msi.core.admin.edit.user.info', array('%username%' => $userToEdit->getUsername()), 'Admin')
+            );
+
+            return $this->redirect($url);
+        }
+        return $this->render('MSIUserBundle:Profile:edit_user.html.twig', array('form' => $formUser->createView(),
+                    'formAvatar' => $formAvatar->createView(),
+                    'formPwd' => $formPwd->createView(),
+                    'userToEdit' => $userToEdit,
+                    'edit_avatar' => true));
     }
-    
+
+    public function editPasswordAction(Request $request, $id) {
+        $userManager = $this->container->get('fos_user.user_manager');
+        $userToEdit = $userManager->findUserBy(array('id' => $id));
+
+        $formUser = $this->container->get('msi_user.edit.form.factory')->createForm();
+        $formUser->setData($userToEdit);
+        $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
+        $formAvatar->setData($userToEdit);
+        //init the user password edit form
+        $formPwd = $this->container->get('msi_user.password.form.factory')->createForm();
+        $formPwd->setData($userToEdit);
+
+        if ($formPwd->handleRequest($request)->isValid()) {
+            $userManager = $this->container->get('fos_user.user_manager');
+            $userManager->updateUser($userToEdit);
+            $url = $this->generateUrl('msi_user_admin_edit_avatar_user', array('id' => $userToEdit->getId()));
+            $session = $request->getSession();
+            $session->getFlashBag()->add('info_user_edit', $this->get('translator')->trans('msi.core.admin.edit.user.pwd.info', array('%username%' => $userToEdit->getUsername()), 'Admin')
+            );
+            return $this->redirect($url);
+        }
+
+        return $this->render('MSIUserBundle:Profile:edit_user.html.twig', array('form' => $formUser->createView(),
+                    'formAvatar' => $formAvatar->createView(),
+                    'formPwd' => $formPwd->createView(),
+                    'userToEdit' => $userToEdit,
+                    'edit_pwd' => true));
+    }
+
 }
--- src/MSI/UserBundle/Controller/ProfileUserController.php
+++ src/MSI/UserBundle/Controller/ProfileUserController.php
@@ -2,42 +2,97 @@
 
 namespace MSI\UserBundle\Controller;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use FOS\UserBundle\Controller\RegistrationController as BaseController;
 use Symfony\Component\HttpFoundation\Request;
-use FOS\UserBundle\FOSUserEvents;
-use FOS\UserBundle\Event\FormEvent;
-use FOS\UserBundle\Event\GetResponseUserEvent;
-use FOS\UserBundle\Event\FilterUserResponseEvent;
-use \MSI\UserBundle\Form\Type\EditUserFormType;
 
-class ProfileUserController extends BaseController
-{
-    
 
-    // path msi_user_profile_edit_avatar
-    public function editProfileAvatarAction($avatarActif, Request $request)
-    {
-          $user = $this->get('security.context')->getToken()->getUser();
-            
-            //init the globale user edit form
-            $formUser = $this->container->get('msi_user.edit.form.factory')->createForm();
-            $formUser->setData($user);
-            //init the user avatar edit form
-            $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
-            $formAvatar->setData($user);
-            if($formAvatar->handleRequest($request)->isValid())
-            {
-                $userManager = $this->container->get('fos_user.user_manager');
-                $userManager->updateUser($user);
-                $url = $this->generateUrl('fos_user_profile_edit');            
-                $session = $request->getSession();
-                $session->getFlashBag()->add('avatar_user_edit', 
-                        $this->get('translator')->trans('msi.core.admin.edit.user.avatar.info',  array('%username%' => $user->getUsername()) , 'Admin')
-                );
-                return $this->redirect($url);
-            }
-            return $this->render('MSIUserBundle:Profile:edit_avatar.html.twig', array('formAvatar' => $formAvatar->createView(),'avatarActif' => $avatarActif));
+class ProfileUserController extends BaseController {
+
+    public function editAction(Request $request) {
+
+        $user = $this->get('security.context')->getToken()->getUser();
+        $formFactory = $this->container->get('msi_user.edit.form.factory');
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
+        $formAvatar->setData($user);
+        //init the user password edit form
+        $formPwd = $this->container->get('msi_user.password.form.factory')->createForm();
+        $formPwd->setData($user);
+
+        // Vérifie si je suis bien en Post et valide le formulaire
+        if ($form->handleRequest($request)->isValid()) {
+            $userManager->updateUser($user);
+            $url = $this->generateUrl('msi_user_edit_profile');
+            $session = $request->getSession();
+            $session->getFlashBag()->add('info_user_edit', $this->get('translator')->trans('msi.core.admin.edit.user.info', array('%username%' => $user->getUsername()), 'Admin')
+            );
+
+            return $this->redirect($url);
+        }
+
+        return $this->render('MSIUserBundle:Profile:edit_profile.html.twig', array('form' => $form->createView(), 
+            'formAvatar' => $formAvatar->createView(), 
+            'formPwd' => $formPwd->createView(), 
+            'user' => $user, 
+            'edit_profile' => true));
     }
-    
+
+    public function editAvatarAction(Request $request) {
+        $user = $this->get('security.context')->getToken()->getUser();
+
+
+        $formUser = $this->container->get('msi_user.edit.form.factory')->createForm();
+        $formUser->setData($user);
+        $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
+        $formAvatar->setData($user);
+        //init the user password edit form
+        $formPwd = $this->container->get('msi_user.password.form.factory')->createForm();
+        $formPwd->setData($user);
+        if ($formAvatar->handleRequest($request)->isValid()) {
+
+            $userManager->updateUser($user);
+            $url = $this->generateUrl('msi_user_edit_avatar_user');
+            $session = $request->getSession();
+            $session->getFlashBag()->add('info_user_edit', $this->get('translator')->trans('msi.core.admin.edit.user.info', array('%username%' => $user->getUsername()), 'Admin')
+            );
+
+            return $this->redirect($url);
+        }
+        return $this->render('MSIUserBundle:Profile:edit_profile.html.twig', array('form' => $formUser->createView(),
+                    'formAvatar' => $formAvatar->createView(),
+                    'formPwd' => $formPwd->createView(),
+                    'user' => $user,
+                    'edit_avatar' => true));
+    }
+
+    public function editPasswordAction(Request $request) {
+       $user = $this->get('security.context')->getToken()->getUser();
+
+        $formUser = $this->container->get('msi_user.edit.form.factory')->createForm();
+        $formUser->setData($user);
+        $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
+        $formAvatar->setData($user);
+        //init the user password edit form
+        $formPwd = $this->container->get('msi_user.password.form.factory')->createForm();
+        $formPwd->setData($user);
+
+        if ($formPwd->handleRequest($request)->isValid()) {
+            $userManager = $this->container->get('fos_user.user_manager');
+            $userManager->updateUser($user);
+            $url = $this->generateUrl('msi_user_edit_avatar_user');
+            $session = $request->getSession();
+            $session->getFlashBag()->add('info_user_edit', $this->get('translator')->trans('msi.core.admin.edit.user.pwd.info', array('%username%' => $user->getUsername()), 'Admin')
+            );
+            return $this->redirect($url);
+        }
+
+        return $this->render('MSIUserBundle:Profile:edit_profile.html.twig', array('form' => $formUser->createView(),
+                    'formAvatar' => $formAvatar->createView(),
+                    'formPwd' => $formPwd->createView(),
+                    'user' => $user,
+                    'edit_pwd' => true));
+    }
+
 }
