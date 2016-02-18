31d6ff8833fe994314107e56b80b4466e520b5d8
David <dav.michel974@gmail.com>
2016-02-18 11:17:37 +0400

    debug edit profile

--- src/MSI/UserBundle/Controller/ProfileUserController.php
+++ src/MSI/UserBundle/Controller/ProfileUserController.php
@@ -13,6 +13,7 @@ class ProfileUserController extends BaseController {
         $user = $this->get('security.context')->getToken()->getUser();
         $formFactory = $this->container->get('msi_user.edit.form.factory');
         $form = $formFactory->createForm();
+        $form->remove('roles'); // delete the option to choose the role for the profile only in admin panel
         $form->setData($user);
 
         $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
@@ -23,8 +24,9 @@ class ProfileUserController extends BaseController {
 
         // Vérifie si je suis bien en Post et valide le formulaire
         if ($form->handleRequest($request)->isValid()) {
+            $userManager = $this->container->get('fos_user.user_manager');
             $userManager->updateUser($user);
-            $url = $this->generateUrl('msi_user_edit_profile');
+            $url = $this->generateUrl('msi_user_edit_user');
             $session = $request->getSession();
             $session->getFlashBag()->add('info_user_edit', $this->get('translator')->trans('msi.core.admin.edit.user.info', array('%username%' => $user->getUsername()), 'Admin')
             );
@@ -44,6 +46,7 @@ class ProfileUserController extends BaseController {
 
 
         $formUser = $this->container->get('msi_user.edit.form.factory')->createForm();
+        $formUser->remove('roles');
         $formUser->setData($user);
         $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
         $formAvatar->setData($user);
@@ -51,7 +54,7 @@ class ProfileUserController extends BaseController {
         $formPwd = $this->container->get('msi_user.password.form.factory')->createForm();
         $formPwd->setData($user);
         if ($formAvatar->handleRequest($request)->isValid()) {
-
+            $userManager = $this->container->get('fos_user.user_manager');
             $userManager->updateUser($user);
             $url = $this->generateUrl('msi_user_edit_avatar_user');
             $session = $request->getSession();
@@ -71,6 +74,7 @@ class ProfileUserController extends BaseController {
        $user = $this->get('security.context')->getToken()->getUser();
 
         $formUser = $this->container->get('msi_user.edit.form.factory')->createForm();
+        $formUser->remove('roles');
         $formUser->setData($user);
         $formAvatar = $this->container->get('msi_user.avatar.form.factory')->createForm();
         $formAvatar->setData($user);
@@ -81,7 +85,7 @@ class ProfileUserController extends BaseController {
         if ($formPwd->handleRequest($request)->isValid()) {
             $userManager = $this->container->get('fos_user.user_manager');
             $userManager->updateUser($user);
-            $url = $this->generateUrl('msi_user_edit_avatar_user');
+            $url = $this->generateUrl('msi_user_edit_pwd_user');
             $session = $request->getSession();
             $session->getFlashBag()->add('info_user_edit', $this->get('translator')->trans('msi.core.admin.edit.user.pwd.info', array('%username%' => $user->getUsername()), 'Admin')
             );
