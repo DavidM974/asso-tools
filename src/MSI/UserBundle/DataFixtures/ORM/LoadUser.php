5391642f8585421faf5406f1c3f1dd50c5c5e348
Benoit Defrene <luceze@hotmail.fr>
2016-02-23 15:01:39 +0400

    Mis à jour du fixture User

--- src/MSI/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/MSI/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -25,29 +25,33 @@ class LoadUser implements FixtureInterface, ContainerAwareInterface {
         $user1 = $userManager->createUser();
         $user1->setUsername('Superadmin');
         $user1->setEmail('super.admin@gmail.com');
-        $user1->setPassword('Superadmin');
+        $user1->setPlainPassword('admin');
         $user1->setRoles(array('ROLE_SUPER_ADMIN'));
+        $user1->setEnabled(true);
         $userManager->updateUser($user1);
 
         $user2 = $userManager->createUser();
         $user2->setUsername('Admin');
         $user2->setEmail('admin@gmail.com');
-        $user2->setPassword('Admin');
+        $user2->setPlainPassword('Admin');
         $user2->setRoles(array('ROLE_ADMIN'));
+        $user2->setEnabled(true);
         $userManager->updateUser($user2);
 
         $user3 = $userManager->createUser();
         $user3->setUsername('Pasteur');
         $user3->setEmail('pasteur@gmail.com');
-        $user3->setPassword('Pasteur');
+        $user3->setPlainPassword('Pasteur');
         $user3->setRoles(array('ROLE_PASTEUR'));
+        $user3->setEnabled(true);
         $userManager->updateUser($user3);
 
         $user4 = $userManager->createUser();
         $user4->setUsername('Secretaire');
         $user4->setEmail('secretaire@gmail.com');
-        $user4->setPassword('Secretaire');
+        $user4->setPlainPassword('Secretaire');
         $user4->setRoles(array('ROLE_SECRETAIRE'));
+        $user4->setEnabled(true);
         $userManager->updateUser($user4);
     }
 
