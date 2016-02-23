ce5055fff583c75a47f93a5b934c9d73f456b236
Benoit Defrene <luceze@hotmail.fr>
2016-02-23 14:41:00 +0400

    Ajout de la fixture User

---
+++ src/MSI/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace MSI\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use MSI\UserBundle\Entity\User;
+
+class LoadUser implements FixtureInterface, ContainerAwareInterface {
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager) {
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->container->get('fos_user.user_manager');
+        $user1 = $userManager->createUser();
+        $user1->setUsername('Superadmin');
+        $user1->setEmail('super.admin@gmail.com');
+        $user1->setPassword('Superadmin');
+        $user1->setRoles(array('ROLE_SUPER_ADMIN'));
+        $userManager->updateUser($user1);
+
+        $user2 = $userManager->createUser();
+        $user2->setUsername('Admin');
+        $user2->setEmail('admin@gmail.com');
+        $user2->setPassword('Admin');
+        $user2->setRoles(array('ROLE_ADMIN'));
+        $userManager->updateUser($user2);
+
+        $user3 = $userManager->createUser();
+        $user3->setUsername('Pasteur');
+        $user3->setEmail('pasteur@gmail.com');
+        $user3->setPassword('Pasteur');
+        $user3->setRoles(array('ROLE_PASTEUR'));
+        $userManager->updateUser($user3);
+
+        $user4 = $userManager->createUser();
+        $user4->setUsername('Secretaire');
+        $user4->setEmail('secretaire@gmail.com');
+        $user4->setPassword('Secretaire');
+        $user4->setRoles(array('ROLE_SECRETAIRE'));
+        $userManager->updateUser($user4);
+    }
+
+}
